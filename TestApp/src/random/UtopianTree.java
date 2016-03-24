31506bf00f277b8c3d172bfac44e40fe9600951f
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-03-24 17:12:37 -0400

    Utopian Tree Hacker rank

---
+++ TestApp/src/random/UtopianTree.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package random;
+
+/**
+ * @author Sudhansu
+ *
+ */
+public class UtopianTree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UtopianTree u = new UtopianTree();
+		u.findHeight(4);
+
+	}
+	void findHeight(int cycle){
+		int height = 1;
+		if(cycle ==0)
+		{
+			System.out.println(height);
+			return;
+		}
+		
+		while(cycle>0){
+			if(cycle>0)
+				height = 2*height;
+			cycle --;
+			if(cycle>0)
+				height++;
+			cycle--;
+		}
+		System.out.println(height);
+
+	}
+}

d248a7eb214a96c8251e813a9209bce6fb34c0c0
Sudhansu Singh <sudhansusingh22@gmail.com>
2016-08-02 17:04:58 -0400

    longest substring in a string
    "stackoverflow" > "stackoverfl"

---
+++ TestApp/src/LongestSubstring.java
@@ -0,0 +1,30 @@
+import java.lang.Math; // header stuff MUST go above the first class
+import java.util.*;
+import java.util.Arrays;
+// our main class becomes a file but the main method is still found
+public class LongestSubstring
+{
+ 	public static void main(String[] args) {
+	    findLongestSub("ABDEFGABEF");
+	}
+	
+	public static void findLongestSub(String str){
+	    HashSet<Character> hs = new HashSet<Character>();
+	    char[] charArray = str.toCharArray();
+	    String l = "";
+	    String o = "";
+	    for(char c: charArray){
+	        if(hs.contains(c)){
+	            l = "";
+	            hs.clear();
+	        }
+	        l += c;
+	        if(l.length()>o.length()){
+	            o = l;
+	        }
+	        hs.add(new Character(c));
+	    }
+	    System.out.println(o.length());
+	}
+}
+// this will become its own file too (and these can be in any order)

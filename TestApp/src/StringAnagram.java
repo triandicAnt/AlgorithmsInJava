ada9c491eae37307ba35774a60dd1e7a6c0e746c
Sudhansu Singh <sudhansusingh22@gmail.com>
2016-08-04 16:26:05 -0400

    check string is anagram or not

---
+++ TestApp/src/StringAnagram.java
@@ -0,0 +1,29 @@
+import java.lang.Math; // header stuff MUST go above the first class
+
+public class StringAnagram
+{
+  static int[] arr = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+  public static void main(String[] args)
+  {
+    String str1 = "aab";
+    String str2 = "aba";
+    checkAnagram(str1, str2);
+  }
+  
+  public static void checkAnagram(String str1, String str2){
+    if (primeHash(str1) == primeHash(str2)){
+      System.out.println("anagram");
+    }
+    else{
+            System.out.println("not anagram");
+    }
+  }
+  
+  public static long primeHash(String str){
+   	long hash = 1;
+    for(char c: str.toCharArray()){
+      hash = hash*arr[c-'a'];
+   }
+    return hash;
+  }
+}

f2f81194f00a8d5f044ec10cc690a679a83d0f08
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-08-28 19:05:00 -0400

    remove duplicates from array

---
+++ TestApp/src/array/RemoveDuplicatesArray.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesArray {
+	public static void main(String[] args) {
+		int [] a = {1,2,1,3,1};
+		int [] b = {1,1,1,1,1};
+		System.out.println(Arrays.toString(removeDuplicates(a)));
+		System.out.println(Arrays.toString(removeDuplicates(b)));
+
+	}
+	
+	public static int[] removeDuplicates(int [] a){
+		if (a.length <= 1)
+			return a;
+		int n = a.length;
+		int nL = 1;
+		for(int i = 0; i< n ; i++){
+			int j = 0;
+			for(j = 0;j<nL;j++){
+				if(a[i] == a[j])
+					break;
+			}
+			if (j == nL)
+				a[nL++] = a[i];
+		}
+		for(int i= nL; i< n;i++)
+			a[i] = 0;
+		return a;
+	}
+}

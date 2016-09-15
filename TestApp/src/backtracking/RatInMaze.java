8708bd136406b996c40e2581863c2fff9004234d
Sudhansu <sudhansusingh22@gmail.com>
2016-09-15 19:52:12 -0400

    find all paths in n*n

---
+++ TestApp/src/backtracking/RatInMaze.java
@@ -0,0 +1,90 @@
+package backtracking;
+
+// ref : http://www.geeksforgeeks.org/backttracking-set-2-rat-in-a-maze/
+public class RatInMaze {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	        int maze[][] = {{1, 0, 0, 0},
+	        				{1, 1, 0, 1},
+	        				{0, 1, 0, 0},
+	        				{1, 1, 1, 1}
+	        			   };
+	        solveMaze(maze);
+
+	}
+	
+	// check if the current block is safe or not
+	
+	private static void solveMaze(int[][] maze) {
+		int sol[][] = {{0, 0, 0, 0},
+	            	   {0, 0, 0, 0},
+	                   {0, 0, 0, 0},
+	                   {0, 0, 0, 0}
+	                  };
+		
+		if(mazeUtil(maze, sol, 0, 0)){
+			// print solution
+			printSolution(sol);
+		}else{
+			System.out.println("No solution exist!");
+		}
+		
+	}
+
+	private static boolean mazeUtil(int[][] maze, int[][] sol, int i, int j) {
+		
+		// check if x,y is the goal or not
+		
+		int m = maze.length;
+		int n = maze[0].length;
+		if(i == m-1 && j == n-1){
+			sol[i][j] = 1;
+//			return (maze[i][j] == 1);
+			return true;
+		}
+		
+		// if the current block is safe then proceed 
+		if(isSafe(i, j, maze)){
+			sol[i][j] = 1;
+			//check while moving right
+			if(mazeUtil(maze, sol, i, j+1)){
+				return true;
+			}
+			
+			// check while moving left
+			if(mazeUtil(maze, sol, i+1, j)){
+				return true;
+			}
+			
+			// backtrack
+			sol[i][j] = 0;
+			return false;
+		}
+		
+		return false;
+	}
+
+	static boolean isSafe(int x, int y, int mat[][]){
+		int m = mat.length;
+		int n = mat[0].length;
+		return (x>=0 && x< m &&
+				y>=0 && y<n &&
+				mat[x][y] == 1);
+	}
+	
+	static void printSolution(int sol[][])
+	    {
+		int m = sol.length;
+		int n = sol[0].length;
+	        for (int i = 0; i < m; i++)
+	        {
+	            for (int j = 0; j < n; j++)
+	                System.out.print(" " + sol[i][j] +
+	                                 " ");
+	            System.out.println();
+	        }
+	    }
+
+}
---
+++ TestApp/src/dp/PossiblePathsInMatrix.java
@@ -0,0 +1,79 @@
+package dp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PossiblePathsInMatrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] mat = {{1,2,3,4},
+						{5,6,7,8},
+						{9,10,11,12},
+						{13,14,15,16}
+		};
+		System.out.println(Arrays.deepToString(findPaths(mat)));
+		System.out.println(findPathCount(mat));
+		System.out.println(printAllPaths(mat));
+	}
+	
+	/**
+	 * can move right and left only
+	 * 1  2  3  4
+	 * 5  6  7  8   
+	 * 9 10 11 12
+	 */
+	
+	static int [][] findPaths(int [][] matrix){
+		int m = matrix.length;
+		int n = matrix[0].length;
+		int path [][] = new int [m][m];
+		// first row
+		for(int i=0;i<n;i++){
+			path[0][i] = 1;
+		}
+		// first column
+		for(int i=0;i<m;i++){
+			path[i][0] = 1;
+		}
+		for(int i=1;i<m;i++){
+			for(int j =1; j<n;j++){
+				path[i][j] = path[i-1][j] + path[i][j-1];
+			}
+		}
+		return path;
+	}
+	
+	static int findPathCount(int [][]matrix){
+		return pathRecursive(matrix.length-1, 0,0);
+	}
+	static int pathRecursive(int n, int i, int j){
+		if(i== n || j==n)
+			return 1;
+		return pathRecursive(n, i+1, j) + pathRecursive(n, i, j+1);
+	}
+	
+	static List<String> printAllPaths(int [][]matrix){
+		List<String> pathList = new ArrayList<String>();
+		getPath(matrix.length-1, 0,0,pathList,"");
+		return pathList;
+	}
+	
+	static void getPath(int n, int i, int j, List<String> pathList, String path){
+		// add i,j to path
+		path+= String.format("(%d,%d)", i,j);
+		if(i==n || j==n){
+			pathList.add(path);
+		}
+		// getting outside the boundary
+		else if(i>n || j>n){
+			return;
+		}
+		else{
+			getPath(n, i+1, j, pathList, path);
+			getPath(n, i, j+1, pathList, path);
+		}
+	}
+
+}

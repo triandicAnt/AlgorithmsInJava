f4eb165e2ded48fff9cebd34a6edc5c7e1df03c2
Sudhansu <sudhansusingh22@gmail.com>
2016-09-17 16:35:19 -0400

    all partition of a number

--- TestApp/src/dp/SubsetSumInArray.java
+++ TestApp/src/dp/SubsetSumInArray.java
@@ -13,7 +13,9 @@ public class SubsetSumInArray {
 		
 		int a [] = {1,2,3,4,5,6};
 		int sum = 10;
-		allSubSet(a, 0, 0, sum, "");
+//		allSubSet(a, 0, 0, sum, "");
+		printAll(4, 3, "");
+		
 		
 
 	}
@@ -71,7 +73,7 @@ public class SubsetSumInArray {
 	}
 	
 	/**
-	 * all subsets with a given sum
+	 * all subsets with a given sum (non repeating numbers)
 	 * 
 	 */
 	
@@ -85,6 +87,20 @@ public class SubsetSumInArray {
 			else if(cur + arr[i]<sum){
 				allSubSet(arr, i+1, cur+arr[i], sum, result + " " + arr[i]);
 			}
+		}
+	}
+	
+	/**
+	 * Print all unique partition
+	 */
+	static void printAll(int target, int maxValue, String suffix){
+		if(target == 0)
+			System.out.println(suffix);
+		else{
+			if(maxValue >1)
+				printAll(target, maxValue-1, suffix);
+			if(maxValue<=target)
+				printAll(target-maxValue, maxValue, maxValue + " " +suffix);
 		}
 	}
 }

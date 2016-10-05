2822238459851373ff3bdf4f2dc46afbf9eeac14
Sudhansu <sudhansusingh22@gmail.com>
2016-10-05 16:43:04 -0400

    extract sentences from POS tags

--- TestApp/src/random/ThreadC.java
+++ TestApp/src/random/ThreadC.java
@@ -49,10 +49,6 @@ public class ThreadC {
 //		System.out.println(arr);
 		
 		System.out.println(reverseWords("Hello watson"));
-		System.out.println(testNLP("(NP (NP (DT a) (NN boy)) (VP (VBG eating) (NP (NNS sausages))))"));
-		System.out.println(extractSentence("(NP (NP (DT a) (NN boy)) (VP (VBG eating) (NP (NNS sausages))))"));
-		System.out.println(testNLP("(S (NP (NNP James)) (VP (VBZ is) (NP (NP (DT a) (NN boy)) (VP (VBG eating) (NP (NNS sausages))))))"));
-		System.out.println(extractSentence("(S (NP (NNP James)) (VP (VBZ is) (NP (NP (DT a) (NN boy)) (VP (VBG eating) (NP (NNS sausages))))))"));
 
 	}
 	public static String reverse(String s) {
@@ -76,37 +72,7 @@ public class ThreadC {
 		
 	}
 	
-	public static String testNLP(String str){
-		/*
-		 * 
-(NP (NP (DT a) (NN boy)) (VP (VBG eating) (NP (NNS sausages))))
-(S (NP (NNP James)) (VP (VBZ is) (NP (NP (DT a) (NN boy)) (VP (VBG eating) (NP (NNS sausages))))))
 
-		 */
-		
-		String [] strArray = {"S","NNP","NNS","NP","NP", "DT","VP","VBG", "VBZ", "NN"};
-		String temp = str;
-		for(String a: strArray){
-			if(temp.contains(a))
-				temp = temp.replaceAll(a, " ");
-		}
-//		System.out.println(temp + "------");
-		temp = temp.replaceAll("\\(", " ");
-		temp = temp.replaceAll("\\)", " ");
-
-		return temp.trim().replaceAll(" +", " ");
-	}
-	
-	public static String extractSentence(String str){
-		Pattern regexpPattern = Pattern.compile("([A-Z]?[a-z]+)\\)");
-		Matcher m = regexpPattern.matcher(str);
-		StringBuffer sb = new StringBuffer();
-		while (m.find()) {
-		    sb.append(m.group(1));
-		    sb.append(" ");
-		}
-		return sb.toString();
-	}
 	
 	public static String reverseWords(String s) {
 		if (s == null || s.length() == 0) {

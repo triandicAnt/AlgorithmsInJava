59333b99abeafe8001c28613c40b48139ca10a72
Sudhansu <sudhansusingh22@gmail.com>
2016-10-06 23:53:34 -0400

    longest palindromic substring

--- TestApp/src/string/LongestPalindromicSubstring.java
+++ TestApp/src/string/LongestPalindromicSubstring.java
@@ -1,5 +1,10 @@
 package string;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 public class LongestPalindromicSubstring {
 
 	public static void main(String[] args) {
@@ -9,6 +14,8 @@ public class LongestPalindromicSubstring {
 		System.out.println(lps(s));
 		String s1 = "abcdcb"; // output is bcdcb
 		System.out.println(lps(s1));
+		System.out.println(LongestPalindrome(s));
+		System.out.println(LongestPalindrome(s1));
 
 	}
 	
@@ -42,5 +49,37 @@ public class LongestPalindromicSubstring {
 		return s.substring(startingIndex, startingIndex+maxLength);
 		
 	}
+/**
+ * leet code
+ */
+	
 
+	 public static String LongestPalindrome(final String input) {
+
+	     PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>(){
+	    	 @Override
+	    	  public int compare(String i, String j){
+	   		  if(i.length()>j.length())
+	   			  return -1;
+	   		  if(i.length()<j.length())
+	   			  return 1;
+	   		  else 
+	   			  return 0;
+	   	  }
+	     });
+	     for (int i = 0; i < input.length(); i++) {
+	         // expanding even length palindromes:
+	         expandPalindromes(pq,input,i,i+1);
+	         // expanding odd length palindromes:
+	         expandPalindromes(pq,input,i,i);
+	     } 
+	     return pq.poll();
+	  }
+
+	  public static void expandPalindromes(final PriorityQueue<String> result, final String s, int i, int j) {
+	      while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+	            result.offer(s.substring(i,j+1));
+	            i--; j++;
+	      }
+	  }
 }

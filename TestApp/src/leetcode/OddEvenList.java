d786ddcd8f241302c3df0edcdcb8197ffaf8d89a
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-10-25 23:22:28 -0400

    odd even list

--- TestApp/src/leetcode/OddEvenList.java
+++ TestApp/src/leetcode/OddEvenList.java
@@ -15,47 +15,67 @@ public class OddEvenList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// [1,2,3,4,5,6,7,8]
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		head.next.next.next.next.next = new ListNode(6);
+		head.next.next.next.next.next.next = new ListNode(7);
+		head.next.next.next.next.next.next.next = new ListNode(8);
+		
+		printList(oddEvenList(head));
+
 	}
-	 public ListNode oddEvenList(ListNode head) {
+	 public static ListNode oddEvenList(ListNode head) {
 	        
 	        if(head == null || head.next == null)
 	            return head;
+	        // odd
 	        ListNode cur = head;
 	        ListNode odd = head;
 	        ListNode oddCur = odd;
+	        
+	        //even
 	        ListNode even = head.next;
 	        ListNode evenCur = even;
-	        ListNode curEven = cur.next;
-	        if(curEven.next!=null)
-	            curEven = curEven.next.next;
 	        
 	        cur = cur.next.next;
-	        ListNode oddPrev = cur;
+	        
 	        while(cur!=null){
+	        	
+	        	System.out.println(cur + "++++++=====");
+	        	// set the off list
+	        	
 	            oddCur.next = cur;
-	            oddPrev = oddCur;
 	            oddCur = oddCur.next;
+	            
+	            // set the even list 
+	            System.out.println(cur.val + " " + evenCur.val );
+	            evenCur.next = cur.next;
+	            System.out.println(evenCur.val + "----");
+	            evenCur = evenCur.next;
+	            System.out.println(evenCur + "+++");
+	            System.out.println(cur.next + "***");
+	           // cur = cur.next;
 	            if(cur.next!=null)
 	              cur = cur.next.next;
+	            else{
+	            	cur = cur.next;
+	            	System.out.println("Hallelujah");
+	            }
 	        }
-	        System.out.println(oddCur.val);
-	         while(curEven!=null){
-	            evenCur.next = curEven;
-	            evenCur = evenCur.next;
-	            if(curEven.next!=null)
-	                curEven = curEven.next.next;
-	        }
-
-	       // evenCur.next = null;
-	        //oddCur.next = null;
-
+	       
+	        System.out.println("----" + oddCur.val);
+	        oddCur.next = null;
 	        printList(even);
-	        oddPrev.next = even;
+	        printList(odd);
+	        oddCur.next = even;
 	        return odd;
 	        
 	    }
 	    
-	    public void printList(ListNode head){
+	    public static void printList(ListNode head){
 	        ListNode cur = head;
 	        while(cur!=null){
 	            System.out.print(cur.val + ",");

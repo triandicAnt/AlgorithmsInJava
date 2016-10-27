6589de0d1f27fd53396ed70084222fe93e8b6cad
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-10-27 15:42:58 -0400

    sorted array to BST

---
+++ TestApp/src/leetcode/SortedArrayToBST.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package leetcode;
+
+/**
+ * @author Sudhansu
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+ */
+public class SortedArrayToBST {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	 public TreeNode sortedArrayToBST(int[] nums) {
+	        if(nums == null || nums.length<1)
+	            return null;
+	        // 1,2,3,4,5
+	        TreeNode root = constructTree(nums, 0, nums.length-1);
+	        return root;
+	    }
+	    
+	    public TreeNode constructTree(int []nums, int l, int h){
+	        if(l>h)
+	            return null;
+	        int m = l+(h-l)/2;
+	        TreeNode root = new TreeNode(nums[m]);
+	        root.left = constructTree(nums, l,m-1);
+	        root.right = constructTree(nums, m+1,h);
+	        return root;
+	    }
+}

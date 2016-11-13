b27b9e0fcec8563d66b397754495d3b25ce1a24a
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-11-12 22:19:10 -0500

    right view of BT

---
+++ TestApp/src/leetcode/RightViewBT.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Sudhansu
+ *
+ */
+public class RightViewBT {
+
+	public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list= new ArrayList<Integer>();
+
+        if(root == null)
+            return list;
+        // use the level order traversal of tree
+        List<TreeNode> nodeList = new ArrayList<TreeNode>();
+
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        //list.add(root.val);
+        nodeList.add(root);
+        q.offer(null);
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            if(temp==null){
+                q.offer(null);
+            }else{
+                if(temp.left!=null)
+                    q.offer(temp.left);
+                if(temp.right!=null)
+                    q.offer(temp.right);
+            }
+        }
+        for(int i=1; i<nodeList.size();i++){
+            if(nodeList.get(i)==null)
+                list.add(nodeList.get(i-1).val);
+        }
+        return list;
+    }
+}

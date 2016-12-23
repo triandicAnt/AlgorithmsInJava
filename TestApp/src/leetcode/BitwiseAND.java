d1b2bd021de40c2d20f8dfc1585114423a2ac4c8
Sudhansu Singh <sudhansusingh22@gmail.com>
2016-12-23 12:49:59 -0500

    bitwise and of numbers

---
+++ TestApp/src/leetcode/BitwiseAND.java
@@ -0,0 +1,20 @@
+public class BitwiseAND {
+    public int rangeBitwiseAnd(int m, int n) {
+       /*  int x =m;
+        for(int i=m+1;i<=n;i++){
+            if(x==0)
+                break;
+            x=x&i;
+            //System.out.println(x);
+        }
+        return x; */
+        
+        int x = 0;
+        while(m!=n){
+            m = m>>1;
+            n= n>>1;
+            x+=1;
+        }
+        return m<<x;
+    }
+}

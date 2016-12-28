15283de5ef80ae53c0c0b783111e0ed7139def9a
Sudhansu Singh <sudhansusingh22@gmail.com>
2016-12-28 16:48:31 -0500

    Reverse Linked List I

---
+++ TestApp/src/leetcode/ReverseLinkedListI.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class ReverseLinkedListI {
+    public ListNode reverseList(ListNode head) {
+       /* if(head==null|| head.next == null)
+            return head;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;*/
+        return reverseListRecursive(head);
+    }
+    public ListNode reverseListRecursive(ListNode head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode rest = head.next;
+        head.next = null;
+        ListNode newRoot = reverseListRecursive(rest);
+        rest.next = head;
+        return newRoot;
+    }
+}

29e650ab66bd757a46d7b7e1489415ff4daf810c
Sudhansu Singh <sudhansusingh22@gmail.com>
2016-12-29 13:21:22 -0500

    Repeat DNA Sequence

---
+++ TestApp/src/leetcode/RepeatDNASequence.java
@@ -0,0 +1,22 @@
+public class RepeatDNASequence {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> list = new ArrayList<>();
+        if(s==null || s.length()<10)
+            return list;
+        HashMap<String, Integer> map = new HashMap<>();
+        // check every 10 length sunstring
+        for(int i=0;i<=s.length()-10;i++)
+        {
+            String str = s.substring(i,i+10);
+            if(map.containsKey(str)){
+                map.put(str,map.get(str)+1);
+                if(!list.contains(str) && map.get(str)>1){
+                    list.add(str);
+                }
+            }else{
+                    map.put(str,1);
+                }
+        }
+        return list;
+    }
+}

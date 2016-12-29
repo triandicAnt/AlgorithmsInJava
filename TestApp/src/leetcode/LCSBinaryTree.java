6ff968a06dff08e75f367641e167c3964faafe67
Sudhansu Singh <sudhansusingh22@gmail.com>
2016-12-29 15:22:41 -0500

    LCS BinaryTree

---
+++ TestApp/src/leetcode/LCSBinaryTree.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class LCSBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null || root==p || root==q)
+            return root;
+        TreeNode leftLCA = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightLCA = lowestCommonAncestor(root.right,p,q);
+        if(leftLCA!=null && rightLCA!=null)
+            return root;
+        return leftLCA==null?rightLCA:leftLCA;
+    }
+}

901bf035e1016bb68c0c895b2a8207d80d48955e
Sudhansu Singh <sudhansusingh22@gmail.com>
2016-12-30 14:14:17 -0500

    Remove Duplicate Sorted LL

---
+++ TestApp/src/leetcode/RemoveDuplicateSortedLL.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class RemoveDuplicateSortedLL {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head ==null || head.next == null)
+            return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = head;
+        ListNode next = head.next;
+        ListNode newHead = head;
+
+        while(cur!=null){
+            while(next!=null && cur.val==next.val){
+                cur=next;
+                next=next.next;
+                prev.next = next;
+            }
+
+             if(prev.next==cur){
+                prev=cur;
+            }else{
+                prev.next=next;
+            }
+            cur = next;
+            if(next!=null)
+                next = next.next;
+            
+        }
+        
+        return dummy.next;
+    }
+}

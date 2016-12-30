3738b55f70b4643605eddacd64825f223a149cb6
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-12-30 12:15:05 -0500

    integer replacement

---
+++ TestApp/src/leetcode/IntegerReplacement.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package leetcode;
+
+/**
+ * @author Sudhansu
+ *
+ */
+public class IntegerReplacement {
+	    public int integerReplacement(int n) {
+	        int count = 0;
+	        while(n!=1){
+	            if((n&1)==0)
+	                n = n>>>1;
+	            else if(n==3 || ((n>>>1)&1)==0)
+	                n--;
+	            else
+	                n++;
+	            count++;
+	        }
+	        return count;
+	    }
+}

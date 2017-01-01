dae7f82dd372041dcd8d0a68ff835c4d558ec166
sudhansusingh22 <sudhansusingh22@gmail.com>
2017-01-01 18:10:55 -0500

    math power

---
+++ TestApp/src/leetcode/Q50.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package leetcode;
+
+/**
+ * @author Sudhansu
+ *
+ */
+public class Q50 {
+    public double myPow(double x, int n) {
+        if(x==0)
+            return x;
+        if(n==0)
+            return 1;
+        if(n<0){
+            if( n == Integer.MIN_VALUE){
+                ++n;
+                n = -n;
+                x = 1/x;
+                return x * x * myPow( x*x, n/2 );
+            }
+            n = -n;
+            x = 1/x;
+        }
+        return (n%2==0)?myPow(x*x,n/2):x*myPow(x*x,n/2);
+    }
+}

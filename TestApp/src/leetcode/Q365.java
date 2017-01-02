07ca5ddd8577eb8942c448c01004bafd67304be2
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-01-02 15:41:54 -0500

    water and jug problem

---
+++ TestApp/src/leetcode/Q365.java
@@ -0,0 +1,24 @@
+public class Q365 {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if(x+y<z)
+            return false;
+        if(x==z || y==z|| (x+y)==z)
+            return true;
+        return z%gcdIterative(x,y)==0?true:false;
+    }
+    public int gcd(int x, int y){
+        if(x==0 || y==0)
+            return x+y;
+        return gcd(y,x%y);
+    }
+    public int gcdIterative(int x, int y){
+        if(x==0||y==0)
+            return x+y;
+        while(x!=0&&y!=0){
+            int temp = y;
+            y = x%y;
+            x = temp;
+        }
+        return x+y;
+    }
+}

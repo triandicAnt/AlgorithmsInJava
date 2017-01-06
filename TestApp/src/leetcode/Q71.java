2eaf8f09464e50d54450cc5e2b28b469741fab14
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-01-06 14:42:57 -0500

    Simplify Path

---
+++ TestApp/src/leetcode/Q71.java
@@ -0,0 +1,26 @@
+public class Q71 {
+    public String simplifyPath(String path) {
+        if(path==null || path.length() == 0)
+            return "/";
+        Set<String> set = new HashSet<>();
+        set.add("");
+        set.add(".");
+        set.add("..");
+        Deque<String> stack = new LinkedList<>();
+        for(String str : path.split("/")){
+            if(str.equals("..") && !stack.isEmpty())
+                stack.pop();
+            else if(!set.contains(str)){
+                stack.push(str);
+            }
+        }
+        String absPath = "";
+        for(String s: stack){
+            absPath = "/" + s + absPath;
+        }
+        if(absPath.equals(""))
+            return "/";
+        else
+            return absPath;
+    }
+}

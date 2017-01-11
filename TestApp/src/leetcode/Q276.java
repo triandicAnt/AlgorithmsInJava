23aaa1a085d07590415b63f476b86e617a1423ba
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-01-11 16:09:50 -0500

    paint Fence

---
+++ TestApp/src/leetcode/Q276.java
@@ -0,0 +1,17 @@
+
+public class Q276{
+public static int paintFence(int n , int k){
+        if(n==0)
+            return 0;
+        if(n==1)
+            return k;
+        int sameColor = k;
+        int differentColor = k*(k-1);
+        for(int i=2;i<n;i++){
+            int temp = differentColor;
+            differentColor = (differentColor+sameColor)*(k-1);
+            sameColor = temp;
+        }
+        return differentColor+sameColor;
+    }
+ }

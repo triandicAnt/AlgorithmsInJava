9928da279d7e48f12f8d1550974d3d5442d7040e
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-01-10 21:10:38 -0500

    single missing number

---
+++ TestApp/src/leetcode/Q136.java
@@ -0,0 +1,16 @@
+public class Q136 {
+    public int singleNumber(int[] nums) {
+          int n = nums.length;
+          if(n==1)
+            return nums[0];
+        int x = nums[0];
+        for(int i = 1;i<n;i++){
+            x^=nums[i];
+        }
+        // int y = 1;
+        // for(int i = 2;i<=n;i++){
+        //     y^=i;
+        // }
+        return x;
+    }
+}

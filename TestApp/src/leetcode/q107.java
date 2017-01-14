841ec9e292460e3452e83962446f2e2b3f1afcbd
sudhansusingh22 <sudhansusingh22@gmail.com>
2017-01-14 13:37:33 -0500

    binary tree level order II

---
+++ TestApp/src/leetcode/q107.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Sudhansu
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class q107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(root==null)
+            return list;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        q.offer(null);
+        List<Integer> l = new ArrayList<>();
+        while(!q.isEmpty()){
+            TreeNode temp = q.remove();
+            if(temp==null){
+                list.add(0,new ArrayList<>(l));
+                l.clear();
+                if(q.size()>0)
+                    q.offer(null);
+            }else{
+                l.add(temp.val);
+                if(temp.left!=null)
+                    q.offer(temp.left);
+                if(temp.right!=null)
+                    q.offer(temp.right);
+            }
+            
+        }
+        return list;
+        
+    }
+}

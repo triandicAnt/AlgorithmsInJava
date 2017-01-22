4e4690b34caf4f82b908e1af189b6f9de88df26b
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-01-21 22:42:13 -0500

    delete linked list node

---
+++ TestApp/src/leetcode/Q237.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Q237 {
+    public void deleteNode(ListNode node) {
+        
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}

21bc6fb58dc4704e41fcac551bb8c835babbad76
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-02-13 07:50:37 -0500

    coin change

--- TestApp/src/leetcode/Q322.java
+++ TestApp/src/leetcode/Q322.java
@@ -26,4 +26,24 @@ public class Q322 {
         return total[amount]==Integer.MAX_VALUE-1?-1:total[amount];
         
     }
+ public int coinChange1(int[] coins, int amount) {
+               int n = coins.length;
+        if(amount==0)
+            return 0;
+        if(n==0 || (n==1 && coins[0]>amount))
+            return -1;
+       int []temp = new int [amount+1];
+       Arrays.fill(temp, Integer.MAX_VALUE-1);
+       temp[0] = 0;
+       for(int i = 0; i<n;i++){
+           for(int j = 1;j<=amount;j++){
+               if(j<coins[i])
+                    temp[j] = temp[j];
+                else
+                    temp[j] = Math.min(temp[j], 1+temp[j-coins[i]]);
+           }
+       }
+       return temp[amount]==Integer.MAX_VALUE-1?-1:temp[amount];
+        
+    }
 }

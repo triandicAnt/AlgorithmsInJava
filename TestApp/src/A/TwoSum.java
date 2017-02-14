d182ed08335eaba1b141313ea85d3c2f3340a424
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-02-14 12:38:21 -0500

    two sum

---
+++ TestApp/src/A/TwoSum.java
@@ -0,0 +1,18 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int [] res = new int [2];
+        if(nums==null || nums.length ==0)
+            return res;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int i = 0;
+        for(int a : nums){
+            if(map.containsKey(target-a)){
+                res[1] = i;
+                res[0] = map.get(target-a);
+            }
+            map.put(a,i);
+            i++;
+        }
+        return res;
+    }
+}

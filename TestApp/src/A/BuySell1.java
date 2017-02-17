13a62e861ac43a73c3c2ad06f215d497ade9b4bb
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-02-17 10:26:45 -0500

    Best time to buy and sell

---
+++ TestApp/src/A/BuySell1.java
@@ -0,0 +1,15 @@
+//Q121
+public class BuySell1 {
+    public int maxProfit(int[] prices) {
+        if(prices==null || prices.length==0)
+            return 0;
+        int max=0;
+        int min = prices[0];
+        for(int i =1;i<prices.length;i++){
+            max = Math.max(max,prices[i]-min);
+            if(prices[i]<min)
+                min = prices[i];
+        }
+        return max;
+    }
+}

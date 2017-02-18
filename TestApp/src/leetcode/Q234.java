605758cddbdca892b24613f760cea7ba4c893dfc
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-02-18 14:05:23 -0500

    palindrome linked list

--- TestApp/src/leetcode/Q234.java
+++ TestApp/src/leetcode/Q234.java
@@ -44,4 +44,36 @@ public class Q234 {
         
         return true;
     }
+    
+     public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next ==null)
+            return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast!=null)
+            slow = slow.next;
+        
+        slow = reverseList(slow);
+        fast = head;
+        while(slow!=null && fast!=null){
+            if(slow.val!=fast.val)
+                return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    public ListNode reverseList(ListNode head){
+        if(head==null || head.next==null)
+            return head;
+        ListNode rest = head.next;
+        head.next = null;
+        ListNode reversedList = reverseList(rest);
+        rest.next = head;
+        return reversedList;
+    }
 }

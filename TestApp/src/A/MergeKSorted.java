ca48a155608e74a9ef8c304d3fcdd632e38b9625
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-02-18 00:18:17 -0500

    MERGE without PQ

--- TestApp/src/A/MergeKSorted.java
+++ TestApp/src/A/MergeKSorted.java
@@ -32,4 +32,60 @@ public class MergeKSorted {
         }
         return dummy.next;
     }
+      public ListNode mergeKLists(ListNode[] lists) {
+       
+      List<ListNode> arrList = new ArrayList<ListNode>(Arrays.asList(lists)); 
+      return mergeLists(arrList);
+
+    }
+    public ListNode mergeLists(List<ListNode> lists) {
+         if(lists==null || lists.size()==0)
+            return null;
+        if(lists.size()==1)
+            return lists.get(0);
+        else if(lists.size()==2)
+            return mergeTwoList(lists.get(0),lists.get(1));
+        else{
+            return mergeTwoList(mergeLists(lists.subList(0, lists.size()/2)),mergeLists(lists.subList(lists.size()/2,lists.size())));
+        }
+    }
+    
+    public ListNode mergeTwoList(ListNode l1, ListNode l2){
+        if(l1==null)
+            return l2;
+        if(l2==null)
+            return l1;
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        ListNode head ;
+        if(cur1.val>cur2.val){
+            head = cur2;
+            cur2=cur2.next;
+        }else{
+            head = cur1;
+            cur1 = cur1.next;
+        }
+        ListNode cur = head;
+        while(cur1!=null && cur2!=null){
+           if(cur1.val>cur2.val){
+                cur.next = cur2;
+                cur2=cur2.next;
+            }else{
+                cur.next = cur1;
+                cur1 = cur1.next;
+            }
+            cur = cur.next;
+        }
+        while(cur1!=null){
+            cur.next = cur1;
+            cur = cur.next;
+            cur1 = cur1.next;
+        }
+        while(cur2!=null){
+            cur.next = cur2;
+            cur = cur.next;
+            cur2 = cur2.next;
+        }
+        return head;
+    }
 }

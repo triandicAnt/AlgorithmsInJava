d02c7435eb0bef3b7ae10a0ea73a732a7ca3961b
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-02-19 12:27:23 -0500

    Excel sheet Col

---
+++ TestApp/src/leetcode/Q171.java
@@ -0,0 +1,11 @@
+public class Q171 {
+    public int titleToNumber(String s) {
+        if(s==null || s.length()==0)
+            return 0;
+        int res = 0;
+        for(int i = 0; i<s.length();i++){
+            res = res *26 + (s.charAt(i)-'A' +1);
+        }
+        return res;
+    }
+}

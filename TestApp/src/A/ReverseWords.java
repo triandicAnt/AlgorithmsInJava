91b13c838040dc786df5d6e12f22ffd9eef3963b
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-02-19 00:28:20 -0500

    reverse words

--- TestApp/src/A/ReverseWords.java
+++ TestApp/src/A/ReverseWords.java
@@ -23,6 +23,27 @@ public class ReverseWords {
         
     }
     
+      
+    public String reverseWords(String s) {
+        if(s==null)
+            return s;
+        s = s.trim();
+        if(s.length()==0)
+            return "";
+        int i =0;
+        int n = s.length();
+        for(int j = 0; j<n;j++){
+            if(s.charAt(j) == ' '){
+                s = reverse(s,i,j-1);
+                i = j+1;
+            }
+        }
+        s = reverse(s,i,n-1);
+        s = reverse(s, 0, n-1);
+        return s.replaceAll(" +", " ");
+        
+    }
+    
     public String reverse(String s, int l, int r){
         if(l>=r)
             return s;

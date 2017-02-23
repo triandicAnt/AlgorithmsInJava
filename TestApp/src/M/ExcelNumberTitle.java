a5195d7fc6e76e8539b8544e1d73448f3b601aa4
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-02-23 00:19:14 -0500

    column number to title

---
+++ TestApp/src/M/ExcelNumberTitle.java
@@ -0,0 +1,11 @@
+public class ExcelNumberTitle {
+    public String convertToTitle(int n) {
+        StringBuffer sb = new StringBuffer();
+        while(n>0){
+            n--;
+            sb.insert(0, (char)('A' + n%26));
+            n/=26;
+        }
+        return sb.toString();
+    }
+}

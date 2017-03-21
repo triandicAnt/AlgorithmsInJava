3e9e8d07355b702b08be586133357bafbf40f42e
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-03-21 14:58:23 -0400

    combinations

--- TestApp/src/Apple/CombinationString.java
+++ TestApp/src/Apple/CombinationString.java
@@ -12,6 +12,8 @@ class CombinationString{
     Set<String> s = new HashSet<String>(list);
     System.out.println(list);
     System.out.println(list.size() + " " + s.size());
+    String str1 = "abc";
+    System.out.println(combination1(str1.toCharArray()));
   }
   
   public static List<String> combination(char []arr){
@@ -22,6 +24,15 @@ class CombinationString{
     Arrays.sort(arr);
     boolean visited[] = new boolean[arr.length];
     backtrack(list, new ArrayList<Character>(), visited,arr);
+    return list;
+  }
+   public static List<String> combination1(char []arr){
+    
+    List<String> list = new ArrayList<>();
+    if(arr==null || arr.length==0)
+      return list;
+    Arrays.sort(arr);
+    backtrack1(list, new ArrayList<Character>(),arr);
     return list;
   }
   
@@ -47,4 +58,24 @@ class CombinationString{
       }
     }
   }
+
+  
+  public static void backtrack1(List<String> list, List<Character> temp, char []a){
+    if(temp.size()==a.length)
+    {
+      StringBuffer sb = new StringBuffer();
+      for(char c: temp){
+        sb.append(c);
+      }
+      list.add(sb.toString());
+      return;
+    }
+    for(int i = 0;i<a.length;i++){
+        if(temp.contains(a[i]))
+          continue;
+        temp.add(a[i]);
+        backtrack1(list,temp, a);
+        temp.remove(temp.size()-1);
+      }
+    }
 }

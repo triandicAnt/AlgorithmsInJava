422bd83aa0cc6e61ddb732da15c36ca48babd2d0
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-03-24 18:54:42 -0400

    sum of two numbers

---
+++ TestApp/src/top/Q371.java
@@ -0,0 +1,10 @@
+public class Q371 {
+    public int getSum(int a, int b) {
+        while(b!=0){
+            int carry = a&b;
+            a = a^b;
+            b = carry<<1;
+        }
+        return a;
+    }
+}

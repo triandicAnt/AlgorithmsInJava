6c697b16329768898f1ce394eb8e1e3103b0d33b
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-04-08 19:06:50 -0400

    Get random

---
+++ TestApp/src/top/Q380.java
@@ -0,0 +1,52 @@
+public class Q380{
+}
+
+class RandomizedSet {
+    List<Integer> list;
+    Map<Integer, Integer> map;
+    Random r;
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        list = new ArrayList<>();
+       map = new HashMap<>();
+      r = new Random();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(map.containsKey(val))
+            return false;
+       map.put(val, list.size());
+      list.add(val);
+      return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!map.containsKey(val))
+            return false;
+       int loc = map.get(val);
+      if(loc!=list.size()-1){
+        // replace with last element
+        int temp = list.get(list.size()-1);
+        list.set(loc, temp);
+        map.put(temp,loc);
+      }
+      list.remove(list.size()-1);
+      map.remove(val);
+      return true;
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(r.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */

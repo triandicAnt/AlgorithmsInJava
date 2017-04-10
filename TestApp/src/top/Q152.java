9335bbd85784bf6d6fcc7eef5f3c27ec94739fd1
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-04-09 20:21:14 -0400

    Maximum product subarray

---
+++ TestApp/src/top/Q152.java
@@ -0,0 +1,19 @@
+public class Q152 {
+    public int maxProduct(int[] nums) {
+        if(nums==null || nums.length==0)
+            return 0;
+        int maxHerePre = nums[0];
+        int minHerePre = nums[0];
+        int maxSoFar = nums[0];
+        int maxHere, minHere;
+        for(int i = 1; i<nums.length;i++){
+            maxHere = Math.max(Math.max(maxHerePre*nums[i], minHerePre*nums[i]),nums[i]);
+            minHere = Math.min(Math.min(maxHerePre*nums[i], minHerePre*nums[i]),nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxHere);
+            maxHerePre = maxHere;
+            minHerePre = minHere;
+
+        }
+        return maxSoFar;
+    }
+}

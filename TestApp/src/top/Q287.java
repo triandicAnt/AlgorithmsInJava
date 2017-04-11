1eb2928abbacf8256bc3b9cda3a4d84cf7f78ce1
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-04-11 18:42:59 -0400

    find the duplicate number

---
+++ TestApp/src/top/Q287.java
@@ -0,0 +1,18 @@
+public class Q287 {
+    public int findDuplicate(int[] nums) {
+        if(nums==null || nums.length==0)
+            return -1;
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(fast!=slow){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+    }
+}

e49974e94891bb86c258c81488f6e8c55004d739
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-04-11 19:06:13 -0400

    Diameter of binary tree

---
+++ TestApp/src/leetcode/Q543.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Q543 {
+    int max;
+    public int diameterOfBinaryTree(TreeNode root) {
+       maxDepth(root);
+       return max;
+    }
+    public int maxDepth(TreeNode root){
+         if(root==null)
+            return 0;
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        max = Math.max(max, left+right);
+        return Math.max(left,right)+1;
+    }
+}

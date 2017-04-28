d9d6d0e5e5a056febe47cdb622e55f171576d8ed
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-04-28 13:12:38 -0400

    Jump Game

---
+++ TestApp/src/top/Q55.java
@@ -0,0 +1,13 @@
+public class Q55 {
+    public boolean canJump(int[] nums) {
+        if(nums==null || nums.length==0)
+            return false;
+        int max = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i>max)
+                return false;
+            max =Math.max(max, i+nums[i]);
+        }
+        return true;
+    }
+}

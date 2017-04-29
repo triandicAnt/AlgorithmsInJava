29f150c68b86303880b4c92378620ae9c844c5ed
Sudhansu Singh <sudhansusingh22@gmail.com>
2017-04-29 13:44:55 -0400

    regex matching

--- TestApp/src/top/Q10.java
+++ TestApp/src/top/Q10.java
@@ -1,28 +1,30 @@
-public class Q10 {
+public class Solution {
     public boolean isMatch(String s, String p) {
-        if(s==null || p==null)
-            return true;
+        if(s==null || p == null)
+            return false;
         int m = s.length();
         int n = p.length();
-        boolean [][] T = new boolean [m+1][n+1];
-        T[0][0] = true;
-        // handle a*, a*b*
-        for(int i = 1; i<=n;i++){
-            if(p.charAt(i-1) == '*')
-            T[0][i] = T[0][i-2];
+        boolean [][]temp = new boolean[m+1][n+1];
+        temp[0][0] = true;
+        for(int i = 2; i<=n;i++){
+            if(p.charAt(i-1)=='*')
+                temp[0][i] = temp[0][i-2];
         }
         for(int i = 1; i<=m;i++){
             for(int j = 1; j<=n;j++){
-                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1)=='.')
-                    T[i][j] = T[i-1][j-1];
+                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.')
+                    temp[i][j] = temp[i-1][j-1];
                 else if(p.charAt(j-1) == '*'){
-                    boolean flag = T[i][j-2];
-                    if(s.charAt(i-1)==p.charAt(j-2) || p.charAt(j-2) == '.')
-                        flag = flag || T[i-1][j];
-                    T[i][j] = flag;
+                    boolean flag = false;
+                    if(j>1){
+                        flag = temp[i][j-2];
+                        if(s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.')
+                        flag |= temp[i-1][j];
+                    }
+                    temp[i][j] = flag;
                 }
             }
         }
-        return T[m][n];
+        return temp[m][n];
     }
 }
